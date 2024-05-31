f1b9cc9eed111bd29027ba0ef2046e236359c9c1
Fran-Britschgi <129128485+Fran-Britschgi@users.noreply.github.com>
2024-05-31 16:53:29 +0000

    change to value

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,16 +22,16 @@ st.title("Python Code Executor")
 
 st.write("Enter your Python code below:")
 
-response_dict = code_editor.code_editor("print('hi')")
+response_dict = code_editor.code_editor(value = "print('hi')")
 
 #code_input = st.text_area("Python Code", height=200, value="print('hello')")
 if st.button("Execute"):
     try:
         # Redirect output to the Streamlit interface
         exec_globals = {'session': session}
-        exec(response_dict, exec_globals)
-        #st.code(response_dict['text'], language=response_dict['lang'])
+        exec(response_dict['text'], exec_globals)
+        st.code(response_dict['text'], language=response_dict['lang'])
         st.success("Code executed successfully!")
     except Exception as e:
-        st.code(response_dict,language='python')
+        st.code(response_dict['text'],language='python')
         st.error(f"An error occurred:\n{traceback.format_exc()}")
