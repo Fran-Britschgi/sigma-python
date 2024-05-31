585b8416030573030aeb15f67a331eddc5e4ccd7
Fran-Britschgi <129128485+Fran-Britschgi@users.noreply.github.com>
2024-05-31 04:37:47 +0000

    adding some helper functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,13 +21,15 @@ st.title("Python Code Executor")
 
 st.write("Enter your Python code below:")
 
-code_input = st.text_area("Python Code", height=200)
+code_input = st.text_area("Python Code", height=200, value="my_df = session.table('DATABSE.SCHEMA.TABLE').to_pandas' \nnew_df = my_df *2 \nsession.create_dataframe(new_df).write.mode('overwrite').save_as_table('DATABASE.SCHEMA.NEW_TABLE')")
 
 if st.button("Execute"):
     try:
         # Redirect output to the Streamlit interface
         exec_globals = {'session': session}
         exec(code_input, exec_globals)
+        st.code(code_input,language='python')
         st.success("Code executed successfully!")
     except Exception as e:
+        st.code(code_input,language='python')
         st.error(f"An error occurred:\n{traceback.format_exc()}")
